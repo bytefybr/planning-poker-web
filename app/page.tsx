a96c415f1ec1bc92c804809a1cb3b2a14b87fd17
Nicolas Dezena <ndezena@gmail.com>
2024-03-13 18:59:24 -0300

    feat: socket logical

--- app/page.tsx
+++ app/page.tsx
@@ -1,149 +1,255 @@
 "use client";
 
-import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import io from "socket.io-client";
 
 const socket = io("http://localhost:3001");
 
 export default function Home() {
-  const [messages, setMessages] = useState<string[]>([]);
-  const [newMessage, setNewMessage] = useState("");
+  const [roomId, setRoomId] = useState(
+    new URLSearchParams(window.location.search).get("roomId")
+  );
+  const [username, setUsername] = useState("");
 
-  useEffect(() => {
-    // Listen for incoming messages
-    socket.on("chat message", (message) => {
-      setMessages((prevMessages: string[]) => [...prevMessages, message]);
+  const [roomData, setRoomData] = useState<any>();
+  const [card, setCard] = useState("");
+
+  const [showRessetButton, setShowRessetButton] = useState(false);
+
+  const [showCreateRoomFields, setShowCreateRoomFields] = useState(false);
+
+  const initSocketEvents = useCallback(() => {
+    // Manipuladores de eventos do Socket.IO
+
+    socket.on("roomCreated", (roomId) => {
+      alert(1);
     });
-  }, []);
 
-  const sendMessage = () => {
-    socket.emit("chat message", newMessage);
-    setNewMessage("");
+    socket.on("roomJoined", (roomId) => {
+      console.log("Você entrou na sala:", roomId);
+      alert("Você entrou na sala: " + roomId);
+      setRoomId(roomId);
+    });
+
+    socket.on("roomListUpdate", (data) => {
+      console.log("Sala atualizada:", data);
+
+      if (!roomId) {
+        window.history.pushState(
+          { roomId: data.roomId },
+          "Sala",
+          `?roomId=${data.roomId}`
+        );
+      }
+      setRoomId(data.roomId);
+      setRoomData(data);
+    });
+  }, [roomId]);
+
+  // Função para criar uma nova sala
+  const createRoom = () => {
+    if (username) {
+      socket.emit("createRoom", username);
+    }
   };
 
+  // Função para entrar em uma sala
+  const joinRoom = () => {
+    socket.emit("enterInRoom", roomId, username);
+  };
+
+  // Função para selecionar uma carta
+  const selectCard = (card) => {
+    socket.emit("selectCard", roomId, card);
+    setCard(card);
+  };
+
+  // Função para mostrar todas as cartas
+  const showAllCards = () => {
+    const usersSelected = roomData.users.filter(
+      (user: any) => user.alreadySelected
+    );
+
+    if (usersSelected.length !== roomData.users.length) {
+      alert("Nem todos os jogadores selecionaram suas cartas");
+      return;
+    }
+
+    setShowRessetButton(true);
+    socket.emit("showCards", roomId);
+  };
+
+  const ressetCards = () => {
+    socket.emit("ressetCards", roomId);
+    setShowRessetButton(false);
+    setCard("");
+  };
+
+  useEffect(() => {
+    if (!roomId || roomData || !username) {
+      return;
+    }
+
+    socket.emit("roomUpdate", roomId);
+  }, [roomId, roomData]);
+
+  useEffect(() => {
+    initSocketEvents();
+  }, [initSocketEvents]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Planning Poker</h1>
+      {!roomData && !roomId && (
+        <>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => setShowCreateRoomFields(true)}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            Criar Sala
+          </button>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          {showCreateRoomFields && (
+            <div>
+              <input
+                type="text"
+                placeholder="Seu nome"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+                onClick={createRoom}
+              >
+                Criar
+              </button>
+            </div>
+          )}
+        </>
+      )}
+      <br />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      {!roomData && (
         <div>
-          <h1>Socket messages</h1>
-          <div>
-            {messages.map((message, index) => (
-              <div key={index}>{message}</div>
-            ))}
-          </div>
+          <label>Entrar em uma sala:</label>
           <input
             type="text"
-            value={newMessage}
-            onChange={(e) => setNewMessage(e.target.value)}
+            placeholder="Digite o ID da sala"
+            value={roomId || ""}
+            onChange={(e) => setRoomId(e.target.value)}
+            className="ml-2"
           />
-          <button onClick={sendMessage}>Send</button>
+
+          {roomId && (
+            <input
+              type="text"
+              placeholder="Seu nome"
+              value={username || ""}
+              onChange={(e) => setUsername(e.target.value)}
+              className="mr-2 ml-2"
+            />
+          )}
+          {roomId && username && (
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+              onClick={joinRoom}
+            >
+              Entrar
+            </button>
+          )}
         </div>
+      )}
 
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {roomData && (
+        <>
+          <h4>Link para compartilhar: {window.location.href}</h4>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div className="mt-4 mb-4">
+            Jogadores
+            <ul>
+              {roomData.users.map((user: any) => (
+                <li key={user.socketId}>{user.username}</li>
+              ))}
+            </ul>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <h3>Selecione seu ponto:</h3>
+          <button
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2"
+            onClick={() => selectCard("1")}
+          >
+            1
+          </button>
+          <button
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2"
+            onClick={() => selectCard("2")}
+          >
+            2
+          </button>
+          <button
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2"
+            onClick={() => selectCard("3")}
+          >
+            3
+          </button>
+          <button
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2"
+            onClick={() => selectCard("5")}
+          >
+            5
+          </button>
+          <button
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2"
+            onClick={() => selectCard("8")}
+          >
+            8
+          </button>
+          <br />
+          <br />
+          <button
+            className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+            onClick={showAllCards}
+          >
+            Mostrar Todas as Cartas
+          </button>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          {showRessetButton && (
+            <button
+              className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+              onClick={ressetCards}
+            >
+              Reiniciar pontuação
+            </button>
+          )}
+          <br />
+          <br />
+          <p>Carta Selecionada: {card}</p>
+
+          <p>Quem já selecionou:</p>
+
+          <ul>
+            {roomData.users
+              .filter((user: any) => user.alreadySelected)
+              .map((user: any) => (
+                <li key={user.socketId}>{user.username}</li>
+              ))}
+          </ul>
+
+          {roomData.users[0].numberSelected && (
+            <>
+              <p>Média: {roomData.average}</p>
+              <p>Estimativas Recebidas:</p>
+              <ul>
+                {roomData.users.map((user: any) => (
+                  <li key={user.socketId}>
+                    {user.username}: {user.numberSelected}
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
+        </>
+      )}
+    </div>
   );
 }
