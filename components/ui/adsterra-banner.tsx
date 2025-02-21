3eed5ee5ee07ef53c06c9dfb1885dac4bca6a810
Andre Fortunato <andre.fortunato@mercadolivre.com>
2025-02-21 19:57:46 -0300

    enhancement: adds new pages improvements

--- components/ui/adsterra-banner.tsx
+++ components/ui/adsterra-banner.tsx
@@ -1,34 +1,31 @@
-/* eslint-disable @next/next/no-sync-scripts */
-import Head from 'next/head';
+import Script from "next/script";
 
 const AdsterraBanner = () => {
-  return (
-    <>
-      <Head>
-        <script
-          type="text/javascript"
+    return (
+      <>
+        <Script
+          id="adsterra-options"
           dangerouslySetInnerHTML={{
             __html: `
-              atOptions = {
-                'key' : '00918d65ec046cdcd2377002a5bda1cf',
-                'format' : 'iframe',
-                'height' : 90,
-                'width' : 728,
-                'params' : {}
-              };
+                atOptions = {
+                    'key' : '00918d65ec046cdcd2377002a5bda1cf',
+                    'format' : 'iframe',
+                    'height' : 90,
+                    'width' : 728,
+                    'params' : {}
+                };
             `,
           }}
-        ></script>
-        <script
-          type="text/javascript"
-          src="//www.highperformanceformat.com/00918d65ec046cdcd2377002a5bda1cf/invoke.js"
-        ></script>
-      </Head>
-      <div id="ad-container">
-        {/* O seu conteúdo do anúncio pode ser injetado automaticamente pela lógica no invoke.js */}
-      </div>
-    </>
-  );
+        />
+        <Script
+          id="adsterra-script"
+          async
+          src={`https://www.highperformanceformat.com/00918d65ec046cdcd2377002a5bda1cf/invoke.js`}
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </>
+    );
 };
 
 export default AdsterraBanner;
